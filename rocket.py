98daf1788a7246363e692ea73b07cd0a5ee63787
i-m-foster <irenefoster64@gmail.com>
2023-05-09 23:07:00 -0400

    deleting file

--- rocket.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from sklearn.linear_model import RidgeClassifierCV
-from sktime.datasets import load_arrow_head  # univariate dataset
-from sktime.transformers.series_as_features.rocket import Rocket
-
